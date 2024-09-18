acdc617a948871d32d1629398c17918bb75039b1
eureu <venekbespalov@mail.ru>
2024-09-18 14:41:27 +0300

    upd: Footer

--- green-code/src/components/Footer.jsx
+++ green-code/src/components/Footer.jsx
@@ -1,7 +1,13 @@
 export default function Footer() {
   return (
     <footer
-      style={{ height: "100px", backgroundColor: "#6FB0A5", width: "100%" }}
+      style={{
+        height: "100px",
+        backgroundColor: "#6FB0A5",
+        width: "100%",
+        position: "fixed",
+        bottom: "0"
+      }}
     >
       <div>
         <span>sign in</span>
