006c6e86675ec352103b7d63589da5dd9f90de70
vvvvtrt <sleim2000@gmail.com>
2024-09-18 21:17:01 +0300

    add new func

--- app/internal/database/db_sqlite3.py
+++ app/internal/database/db_sqlite3.py
@@ -7,7 +7,7 @@ from datetime import datetime
 warnings.simplefilter("always")
 
 
-def create_tables(DB_PATH):
+async def create_tables(DB_PATH):
     try:
         connection = sqlite3.connect(DB_PATH)
         cursor = connection.cursor()
@@ -36,14 +36,27 @@ def create_tables(DB_PATH):
         )
         """)
 
+        cursor.execute("""
+                CREATE TABLE IF NOT EXISTS give_photo (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    image1 TEXT,
+                    image2 TEXT,
+                    image3 TEXT,
+                    type TEXT,
+                    date TEXT,
+                    cord TEXT,
+                    author TEXT,
+                    comment TEXT
+                )
+                """)
+
         connection.commit()
         connection.close()
     except Exception as ex:
         print(f"[ERROR] Dont create db: {ex}")
 
 
-
-def add_park(DB_PATH, name, description, metro, address, cord, image):
+async def add_park(DB_PATH, name, description, metro, address, cord, image):
     connection = None
 
     try:
@@ -55,7 +68,7 @@ def add_park(DB_PATH, name, description, metro, address, cord, image):
         connection.commit()
         connection.close()
     except sqlite3.OperationalError as e:
-        create_tables(DB_PATH)
+        await create_tables(DB_PATH)
     except Exception as _ex:
         warnings.warn(f"Error: {_ex}")
         return "error"
@@ -67,7 +80,7 @@ def add_park(DB_PATH, name, description, metro, address, cord, image):
         return "ok"
 
 
-def add_type_red_book(DB_PATH, name, type, squad, family, description, live_in_park, image):
+async def add_type_red_book(DB_PATH, name, type, squad, family, description, live_in_park, image):
     connection = None
 
     try:
@@ -80,7 +93,7 @@ def add_type_red_book(DB_PATH, name, type, squad, family, description, live_in_p
         connection.close()
 
     except sqlite3.OperationalError as e:
-        create_tables(DB_PATH)
+        await create_tables(DB_PATH)
 
     except Exception as _ex:
         warnings.warn(f"Error: {_ex}")
@@ -93,7 +106,7 @@ def add_type_red_book(DB_PATH, name, type, squad, family, description, live_in_p
         return "ok"
 
 
-def get_park(DB_PATH):
+async def get_park(DB_PATH):
     connection = None
     result = None
 
@@ -103,12 +116,11 @@ def get_park(DB_PATH):
         cursor.execute("SELECT * FROM parks")
         result = cursor.fetchall()
 
-
         if result is None:
             return "Park not found"
 
     except sqlite3.OperationalError as e:
-        create_tables(DB_PATH)
+        await create_tables(DB_PATH)
     except Exception as _ex:
         warnings.warn(f"Error: {_ex}")
         return "error"
@@ -119,7 +131,7 @@ def get_park(DB_PATH):
     return result
 
 
-def get_creatures(DB_PATH, type_id):
+async def get_creatures(DB_PATH, type_id):
     connection = None
     result = None
 
@@ -132,7 +144,7 @@ def get_creatures(DB_PATH, type_id):
             return "Type not found"
 
     except sqlite3.OperationalError as e:
-        create_tables(DB_PATH)
+        await create_tables(DB_PATH)
     except Exception as _ex:
         warnings.warn(f"Error: {_ex}")
         return "error"
@@ -141,6 +153,131 @@ def get_creatures(DB_PATH, type_id):
             connection.close()
 
     return result
+
+
+async def get_plants(DB_PATH):
+    connection = None
+    result = None
+
+    try:
+        connection = sqlite3.connect(DB_PATH)
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM types_red_book WHERE type = 'plant'")
+        result = cursor.fetchall()
+
+        if result is None:
+            return "Park not found"
+
+    except sqlite3.OperationalError as e:
+        await create_tables(DB_PATH)
+    except Exception as _ex:
+        warnings.warn(f"Error: {_ex}")
+        return "error"
+    finally:
+        if connection:
+            connection.close()
+
+    return result
+
+
+async def get_animals(DB_PATH):
+    connection = None
+    result = None
+
+    try:
+        connection = sqlite3.connect(DB_PATH)
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM types_red_book WHERE type = 'animal'")
+        result = cursor.fetchall()
+
+        if result is None:
+            return "Park not found"
+
+    except sqlite3.OperationalError as e:
+        await create_tables(DB_PATH)
+    except Exception as _ex:
+        warnings.warn(f"Error: {_ex}")
+        return "error"
+    finally:
+        if connection:
+            connection.close()
+
+    return result
+
+
+async def get_mushroom(DB_PATH):
+    connection = None
+    result = None
+
+    try:
+        connection = sqlite3.connect(DB_PATH)
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM types_red_book WHERE type = 'mushroom'")
+        result = cursor.fetchall()
+
+        if result is None:
+            return "Park not found"
+
+    except sqlite3.OperationalError as e:
+        await create_tables(DB_PATH)
+    except Exception as _ex:
+        warnings.warn(f"Error: {_ex}")
+        return "error"
+    finally:
+        if connection:
+            connection.close()
+
+    return result
+
+
+async def add_new_photo(DB_PATH, image1, image2, image3, type, date, cord, author, comment):
+    connection = None
+
+    try:
+        connection = sqlite3.connect(DB_PATH)
+        cursor = connection.cursor()
+        cursor.execute("""
+        INSERT INTO give_photo (image1, image2, image3, type, date, date, cord, author, comment) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        """, (image1, image2, image3, type, date, date, cord, author, comment))
+        connection.commit()
+        connection.close()
+
+    except sqlite3.OperationalError as e:
+        await create_tables(DB_PATH)
+
+    except Exception as _ex:
+        warnings.warn(f"Error: {_ex}")
+        return "error"
+
+    finally:
+        if connection:
+            connection.close()
+
+        return "ok"
+
+
+async def get_photo(DB_PATH):
+    connection = None
+
+    try:
+        connection = sqlite3.connect(DB_PATH)
+        cursor = connection.cursor()
+        cursor.execute("""SELECT * FROM give_photo""")
+        connection.commit()
+        connection.close()
+
+    except sqlite3.OperationalError as e:
+        await create_tables(DB_PATH)
+
+    except Exception as _ex:
+        warnings.warn(f"Error: {_ex}")
+        return "error"
+
+    finally:
+        if connection:
+            connection.close()
+
+        return "ok"
 
 
 if __name__ == '__main__':
--- app/internal/routes/user.py
+++ app/internal/routes/user.py
@@ -21,7 +21,6 @@ class Get_creaturesID(BaseModel):
     id: int
 
 
-
 class Add_park(BaseModel):
     name: str
     description: str
@@ -41,22 +40,50 @@ class Add_creatures(BaseModel):
     image: str
 
 
+class Add_new_photo(BaseModel):
+    name: str
+    type: str
+    squad: str
+    family: str
+    description: str
+    list_park: list
+    image: str
+
 @router.get("/get_parks")
 async def get_parks():
-    return db.get_park(bd_name)
+    return {"data": await db.get_park(bd_name)}
 
+
+@router.get("/get_animals")
+async def get_animals():
+    return {"data": await db.get_animals(bd_name)}
+
+
+@router.get("/get_mushroom")
+async def get_mushroom():
+    return {"data": await db.get_mushroom(bd_name)}
+
+
+@router.get("/get_photo")
+async def get_creatures():
+    return {"data": await db.get_photo(bd_name)}
 
 @router.post("/get_creatures")
 async def get_creatures(data: Get_creaturesID):
-    return db.get_creatures(bd_name, data.id)
+    return {"data": await db.get_creatures(bd_name, data.id)}
+
+
+@router.post("/add_new_photo")
+async def add_park(data: Add_park):
+    return {"data": await db.add_park(bd_name, data.name, data.description, data.metro, data.address, data.cord, data.image)}
 
 
 @router.post("/add_park")
 async def add_park(data: Add_park):
-    return db.add_park(bd_name, data.name, data.description, data.metro, data.address, data.cord, data.image)
+    return {"data": await db.add_park(bd_name, data.name, data.description, data.metro, data.address, data.cord, data.image)}
 
 
 @router.post("/add_creatures")
 async def add_creatures(data: Add_creatures):
-    return db.add_type_red_book(bd_name, data.name, data.type, data.squad, data.family, data.description,
-                                data.list_park, data.image)
+    return {"data": await db.add_type_red_book(bd_name, data.name, data.type, data.squad, data.family, data.description,
+                                      data.list_park, data.image)}
