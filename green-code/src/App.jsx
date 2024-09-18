b44caaae1e94bf9d6cbdaa9631a7fbade0bcf0fd
eureu <venekbespalov@mail.ru>
2024-09-18 19:25:38 +0300

    upd: parks routing

--- green-code/src/App.jsx
+++ green-code/src/App.jsx
@@ -3,12 +3,13 @@ import { Link, Route, Routes } from "react-router-dom";
 import AppHeader from "./components/AppHeader.jsx";
 import NotFoundPage from "./components/NotFoundPage/NotFoundPage.jsx";
 import MainPage from "./components/Pages/Main/Main.tsx";
+import Parks from "./components/Parks.jsx";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
-      <Route path="/parks" element={<AppHeader />} />
+      <Route path="/parks" element={<Parks />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
